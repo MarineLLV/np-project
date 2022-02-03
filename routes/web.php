37822271e6274e612f0b63f7412fc1fc0ba50108
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-03 23:27:35 +0100

    metadata

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $files = File::files(resource_path("posts/"));
+
+    $posts = array_map(function ($file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        return new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug,
+        );
+    }, $files);
+
+    //ddd($posts[0]->title);
+    
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
