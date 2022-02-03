014d16fe1aa817a1ea55dbc88494cf958d589b8f
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-03 10:44:43 +0100

    use caching

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if(!file_exists($path)) {
+    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         //ddd('file does not exist');
         //abort(404);
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
 
+    
     return view('post', [
         'post' => $post
     ]);
