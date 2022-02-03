eb429b4b7193990b9b986df91e0526ddf02dad1f
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-03 12:02:16 +0100

    use the filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,53 +8,10 @@
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-
-    <h1><a href="/posts/my-first-post"> My first post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid excepturi quas nulla sint! Nobis pariatur, rem molestias eum, voluptates harum nam quidem dolorum voluptatum laudantium ipsum dolores atque eveniet!
-    Delectus, eius aperiam. Odit iure maiores porro qui animi, in consequatur modi fugiat accusantium vel dolorem, necessitatibus quasi veniam obcaecati tempora odio laudantium fuga ipsum neque est nobis quaerat! Magni.
-    Reiciendis amet obcaecati excepturi accusamus non? Nostrum sequi officia iusto. Vel ullam error adipisci nihil maxime porro eligendi debitis repudiandae, itaque dolorem! Nostrum adipisci sequi dolores. Veniam corrupti totam animi.
-    Cum molestiae ducimus doloribus magni voluptas incidunt amet quaerat eaque, dolor quos nisi, nam tempora iusto quis, est soluta voluptatem. Minima earum adipisci vero, magnam perspiciatis cumque sed vel laudantium!
-    Quia nesciunt facere doloribus asperiores ab exercitationem porro laboriosam quis odit magni, impedit doloremque commodi? Eum, esse quaerat molestiae quis accusantium deserunt asperiores atque qui molestias ullam in expedita aperiam?
-    Labore incidunt quasi eius dolor nam illo nobis alias necessitatibus quidem, ut reprehenderit voluptate natus minus totam. Porro, tempore ullam voluptatum accusamus cum non totam facilis iste similique excepturi vel!
-    Non, consequatur, beatae voluptatum, inventore culpa dolores quos numquam repellat rem commodi vitae deleniti provident. Nemo similique quo asperiores quisquam praesentium animi consequuntur distinctio ipsum? Perspiciatis exercitationem libero dolores quos?
-    Eaque dolores ullam animi, temporibus aut minus cum quis eligendi? Corrupti modi neque veritatis iusto pariatur illo dolores saepe placeat delectus, sapiente culpa velit sit est doloremque, assumenda dolore sed.
-    Reprehenderit eveniet, nisi corporis, officiis adipisci dicta deserunt fuga eius animi facilis numquam dolores? Porro at numquam aliquid. Numquam ex adipisci at incidunt quaerat aliquam provident laudantium saepe voluptatum aut?
-    Rem necessitatibus ipsam quod deserunt blanditiis? Vel id dolorum expedita, quos distinctio repellendus ipsam voluptas alias repellat, libero mollitia doloribus recusandae fugiat. Quae beatae praesentium esse error blanditiis dolore itaque!</p>
+        <?= $post; ?>
     </article>
-
-    <article>
-
-    <h1><a href="/posts/my-second-post">My second post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid excepturi quas nulla sint! Nobis pariatur, rem molestias eum, voluptates harum nam quidem dolorum voluptatum laudantium ipsum dolores atque eveniet!
-    Delectus, eius aperiam. Odit iure maiores porro qui animi, in consequatur modi fugiat accusantium vel dolorem, necessitatibus quasi veniam obcaecati tempora odio laudantium fuga ipsum neque est nobis quaerat! Magni.
-    Reiciendis amet obcaecati excepturi accusamus non? Nostrum sequi officia iusto. Vel ullam error adipisci nihil maxime porro eligendi debitis repudiandae, itaque dolorem! Nostrum adipisci sequi dolores. Veniam corrupti totam animi.
-    Cum molestiae ducimus doloribus magni voluptas incidunt amet quaerat eaque, dolor quos nisi, nam tempora iusto quis, est soluta voluptatem. Minima earum adipisci vero, magnam perspiciatis cumque sed vel laudantium!
-    Quia nesciunt facere doloribus asperiores ab exercitationem porro laboriosam quis odit magni, impedit doloremque commodi? Eum, esse quaerat molestiae quis accusantium deserunt asperiores atque qui molestias ullam in expedita aperiam?
-    Labore incidunt quasi eius dolor nam illo nobis alias necessitatibus quidem, ut reprehenderit voluptate natus minus totam. Porro, tempore ullam voluptatum accusamus cum non totam facilis iste similique excepturi vel!
-    Non, consequatur, beatae voluptatum, inventore culpa dolores quos numquam repellat rem commodi vitae deleniti provident. Nemo similique quo asperiores quisquam praesentium animi consequuntur distinctio ipsum? Perspiciatis exercitationem libero dolores quos?
-    Eaque dolores ullam animi, temporibus aut minus cum quis eligendi? Corrupti modi neque veritatis iusto pariatur illo dolores saepe placeat delectus, sapiente culpa velit sit est doloremque, assumenda dolore sed.
-    Reprehenderit eveniet, nisi corporis, officiis adipisci dicta deserunt fuga eius animi facilis numquam dolores? Porro at numquam aliquid. Numquam ex adipisci at incidunt quaerat aliquam provident laudantium saepe voluptatum aut?
-    Rem necessitatibus ipsam quod deserunt blanditiis? Vel id dolorum expedita, quos distinctio repellendus ipsam voluptas alias repellat, libero mollitia doloribus recusandae fugiat. Quae beatae praesentium esse error blanditiis dolore itaque!</p>
-    </article>
-
-    <article>
-
-    <h1><a href="/posts/my-third-post">My third post</a></h1>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aliquid excepturi quas nulla sint! Nobis pariatur, rem molestias eum, voluptates harum nam quidem dolorum voluptatum laudantium ipsum dolores atque eveniet!
-    Delectus, eius aperiam. Odit iure maiores porro qui animi, in consequatur modi fugiat accusantium vel dolorem, necessitatibus quasi veniam obcaecati tempora odio laudantium fuga ipsum neque est nobis quaerat! Magni.
-    Reiciendis amet obcaecati excepturi accusamus non? Nostrum sequi officia iusto. Vel ullam error adipisci nihil maxime porro eligendi debitis repudiandae, itaque dolorem! Nostrum adipisci sequi dolores. Veniam corrupti totam animi.
-    Cum molestiae ducimus doloribus magni voluptas incidunt amet quaerat eaque, dolor quos nisi, nam tempora iusto quis, est soluta voluptatem. Minima earum adipisci vero, magnam perspiciatis cumque sed vel laudantium!
-    Quia nesciunt facere doloribus asperiores ab exercitationem porro laboriosam quis odit magni, impedit doloremque commodi? Eum, esse quaerat molestiae quis accusantium deserunt asperiores atque qui molestias ullam in expedita aperiam?
-    Labore incidunt quasi eius dolor nam illo nobis alias necessitatibus quidem, ut reprehenderit voluptate natus minus totam. Porro, tempore ullam voluptatum accusamus cum non totam facilis iste similique excepturi vel!
-    Non, consequatur, beatae voluptatum, inventore culpa dolores quos numquam repellat rem commodi vitae deleniti provident. Nemo similique quo asperiores quisquam praesentium animi consequuntur distinctio ipsum? Perspiciatis exercitationem libero dolores quos?
-    Eaque dolores ullam animi, temporibus aut minus cum quis eligendi? Corrupti modi neque veritatis iusto pariatur illo dolores saepe placeat delectus, sapiente culpa velit sit est doloremque, assumenda dolore sed.
-    Reprehenderit eveniet, nisi corporis, officiis adipisci dicta deserunt fuga eius animi facilis numquam dolores? Porro at numquam aliquid. Numquam ex adipisci at incidunt quaerat aliquam provident laudantium saepe voluptatum aut?
-    Rem necessitatibus ipsam quod deserunt blanditiis? Vel id dolorum expedita, quos distinctio repellendus ipsam voluptas alias repellat, libero mollitia doloribus recusandae fugiat. Quae beatae praesentium esse error blanditiis dolore itaque!</p>
-    </article>
-
+    <?php endforeach; ?>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,21 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function($slug) {
+    // Find a post by its slug and pass it to a view called "post"
 
-    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        //ddd('file does not exist');
-        //abort(404);
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
-
-    
+    $post = Post::find($slug);
     return view('post', [
         'post' => $post
     ]);
+
 })->where('post','[A-z_\-]+'); //add regExp
