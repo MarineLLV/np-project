673f07f88f59a80c6005e98c6cc71b33a76533e4
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-04 16:48:26 +0100

    tweaks

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-
-class Post 
+class Post
 {
-
     public $title;
 
     public $excerpt;
@@ -20,7 +18,7 @@ class Post
 
     public $slug;
 
-    public function __construct($title, $excerpt, $date, $body, $slug) 
+    public function __construct($title, $excerpt, $date, $body, $slug)
     {
         $this->title = $title;
         $this->excerpt = $excerpt;
@@ -29,26 +27,37 @@ class Post
         $this->slug = $slug;
     }
 
-
-    public static function all() {
+    public static function all()
+    {
         return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts/")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug,
-            ))->sortByDesc('date');
+            return collect(File::files(resource_path('posts/')))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(
+                    fn($document) => new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                    )
+                )
+                ->sortByDesc('date');
         });
     }
 
-
-    public static function find ($slug) {
-
+    public static function find($slug)
+    {
         // of all the blog posts, find the one with a slug that matches the one that was requested
         return static::all()->firstWhere('slug', $slug);
+    }
 
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (!$post) {
+            throw new ModelNotFoundException();
+        }
+        return $post;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,18 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::all(),
     ]);
 });
 
-Route::get('posts/{post}', function($slug) {
+Route::get('posts/{post}', function ($slug) {
     // Find a post by its slug and pass it to a view called "post"
 
-    $post = Post::find($slug);
     return view('post', [
-        'post' => $post
+        'post' => Post::findOrFail($slug),
     ]);
-
-})->where('post','[A-z_\-]+'); //add regExp
+});
