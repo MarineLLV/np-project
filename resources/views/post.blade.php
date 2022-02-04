b39f79da0500a4cd875977e7987ebb49d1175121
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-04 16:19:51 +0100

    Blade layouts : Blade components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,12 @@
-@extends('layout')
+<x-layout>
+    <article>
+        <h1>
+            <?= $post->title;?>
+        </h1>
+        <div>
+            <?= $post->body;?>
+        </div>
 
-@section('content')
-<article>
-    <h1>
-        <?= $post->title;?>
-    </h1>
-    <div>
-        <?= $post->body;?>
-    </div>
-
-    <a href="/">Go Back</a>
-</article>
-@endsection
+        <a href="/">Go Back</a>
+    </article>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,13 @@
-@extends('layout')
+<x-layout>
+    @foreach ($posts as $post)
+    <article>
+        <a href="/posts/{{ $post->slug }}">
+            <h1>{{ $post->title }}</h1>
+        </a>
 
-
-@section('content')
-@foreach ($posts as $post)
-<article>
-    <a href="/posts/{{ $post->slug }}">
-        <h1>{{ $post->title }}</h1>
-    </a>
-
-    <div>
-        {{ $post->excerpt }}
-    </div>
-</article>
-@endforeach
-@endsection
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+    @endforeach
+</x-layout>
