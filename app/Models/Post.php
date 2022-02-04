a7eebec4f866d5773cea7b3fa0a44362e09dab48
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-04 14:11:27 +0100

    metadata 2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
 
 class Post 
 {
@@ -29,20 +31,22 @@ class Post
 
 
     public static function all() {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
+       return collect(File::files(resource_path("posts/")))
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn($document) => new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug,
+        ));
     }
 
 
     public static function find ($slug) {
 
-        if(!file_exists($path = resource_path("posts/{$slug}.html")))  {
-            //abort(404);
-            throw new ModelNotFoundException();
+        // of all the blog posts, find the one with a slug that matches the one that was requested
+        return static::all()->firstWhere('slug', $slug);
 
-        }
-    
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,24 +16,9 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts/"));
-
-    $posts = array_map(function ($file) {
-        $document = YamlFrontMatter::parseFile($file);
-
-        return new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug,
-        );
-    }, $files);
-
-    //ddd($posts[0]->title);
     
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
