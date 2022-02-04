fdecc09387a0c4bc4f2376f66ab018925ff7c5fb
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-04 16:09:22 +0100

    Blade layouts : template inheritance

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-    <link rel="stylesheet" href="app.css">
-</head>
-<body>
-    @foreach ($posts as $post)
-    <article>
-        <a href="/posts/{{ $post->slug }}">
-            <h1>{{ $post->title }}</h1> 
-        </a>
-       
-       <div>
-           {{ $post->excerpt }}
-       </div>
-    </article>
-    @endforeach
-</body>
-</html>
+@extends('layout')
+
+
+@section('content')
+@foreach ($posts as $post)
+<article>
+    <a href="/posts/{{ $post->slug }}">
+        <h1>{{ $post->title }}</h1>
+    </a>
+
+    <div>
+        {{ $post->excerpt }}
+    </div>
+</article>
+@endforeach
+@endsection
