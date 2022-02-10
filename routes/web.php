95c72d5bd819bc292daaf2a466926bb5bdad86d5
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-10 18:45:07 +0100

    show all posts from an author

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,7 @@ Route::get('/', function () {
     }); */
 
     return view('posts', [
-        'posts' => Post::with('category')->get(),
+        'posts' => Post::latest()->with('category', 'author')->get(),
     ]);
 
 });
@@ -39,5 +40,13 @@ Route::get('posts/{post}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+// Show all posts from an author
+Route::get('authors/{author:username}', function (User $author) {
+    //dd($author);
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
