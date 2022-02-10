4405d17de6465ecad37c46e7cca47c11a654099a
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-10 10:54:28 +0100

    install Clockwork + optimizing queries

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // listen to the queries
+    /* \Illuminate\Support\Facades\DB::listen(function ($query) {
+        logger($query->sql, $query->bindings);
+    }); */
+
     return view('posts', [
-        'posts' => Post::all(),
+        'posts' => Post::with('category')->get(),
     ]);
+
 });
 
 // Find a post by its slug and pass it to the view
