237e4c903218c36df3a213c3fe5d0a05fca02af7
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-16 20:44:25 +0100

    grid layouts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,12 @@
+@props(['post'])
+
 <x-layout>
     @include ('_post-header')
-
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts->first()" />
-
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-            <x-post-card :posts="$posts" />
-            @endforeach
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card :posts="$posts" />
-            <x-post-card :posts="$posts" />
-            <x-post-card :posts="$posts" />
-        </div>
+        @if ($posts->count())
+           <x-postsGrid :posts="$posts" />
+        @else
+        <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
